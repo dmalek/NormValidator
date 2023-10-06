6a960a3520b1cdf2cf73f752cbf990584394b521
Dalibor Malek <dalibor.malek@igea.hr>
2023-10-06 16:39:49 +0200

    example wit custom fault properties

--- tests/NormValidator.Test/CompettitionFaults.cs
+++ tests/NormValidator.Test/CompettitionFaults.cs
@@ -2,16 +2,33 @@
 {
     public class CompettitionFaults : FaultType
     {
-        public static CompettitionFaults InvalidData = new CompettitionFaults(nameof(InvalidData));
+        public static CompettitionFaults InvalidData = new CompettitionFaults(nameof(InvalidData), 100);
 
-        public static CompettitionFaults AgeLimit = new CompettitionFaults(nameof(AgeLimit));
+        public static CompettitionFaults AgeLimit = new CompettitionFaults(nameof(AgeLimit), 200);
 
-        public static CompettitionFaults NotSport = new CompettitionFaults(nameof(NotSport));
+        public static CompettitionFaults NotSport = new CompettitionFaults(nameof(NotSport), 300);
 
-        public static CompettitionFaults PlayerSuspended = new CompettitionFaults(nameof(PlayerSuspended));
+        public static CompettitionFaults PlayerSuspended = new CompettitionFaults(nameof(PlayerSuspended), 310);
 
+        #region Custom fault properies
+        public int Code { get; set; }
+        public string  Message { get; set; }
         private CompettitionFaults(string name) : base(name)
         {
-        }    
+        }
+
+        private CompettitionFaults(string name, int code) : base(name)
+        {
+            Code = code;
+            Message = string.Empty;
+        }
+
+        private CompettitionFaults(string name, int code, string message) : base(name)
+        {
+            Code = code;
+            Message = message;
+        }
+
+        #endregion
     }
 }
